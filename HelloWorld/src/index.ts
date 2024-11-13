0b7d9a659d1c384c7063694da170dbac7c6fa2be
Or Ben Naim <orbennaim123@gmail.com>
2024-11-13 14:35:04 +0200

    .

--- HelloWorld/src/index.ts
+++ HelloWorld/src/index.ts
@@ -1,16 +1,16 @@
 import * as readline from 'readline';
 
-class Greeter{
+class Greeter {
 
     // The greeter method defined as static because 
-    static greet(name: string): string{     
+    static greet(name: string): string {
         return `Hello, ${name}!`;
     }
 }
 
 
-function main(): void{
-    
+function main(): void {
+
     // Importing 'readline' to handle user input from the console
     const readLine = readline.createInterface({
         input: process.stdin,
